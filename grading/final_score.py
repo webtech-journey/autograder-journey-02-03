9d4bfa77a0a38143b8d573f892493e7c54880420
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-09 09:19:06 -0300

    Implemented automatic failing if checks for table tags in html.

--- grading/final_score.py
+++ grading/final_score.py
@@ -18,9 +18,11 @@ class Scorer:
     def set_penalty_score(self,filename):
         self.penalty = get_test_results(self.path.getFilePath(filename))
     def set_final_score(self):
-        final_score = (get_score(self.base[0],4) * 0.8 )+(get_score(self.bonus[0],2) * 0.2) - (get_score(self.penalty[0],7) * 0.3)
-        self.final_score = final_score
-        return final_score
+        self.final_score = 0
+        if 'test_penalty.py::test_table_tag_penalty' not in self.penalty[0]:
+            final_score = (get_score(self.base[0],4) * 0.8 )+(get_score(self.bonus[0],2) * 0.2) - (get_score(self.penalty[0],7) * 0.3)
+            self.final_score = final_score
+        return self.final_score
     def get_feedback(self):
         base_dict = {"passed":self.base[0],"failed":self.base[1]}
         bonus_dict = {"passed":self.bonus[0],"failed":self.bonus[1]}
@@ -34,6 +36,6 @@ class Scorer:
         scorer.set_bonus_score(bonus_file)
         scorer.set_penalty_score(penalty_file)
         scorer.set_final_score()
-        sleep(2)
+        sleep(1)
         return scorer
 
