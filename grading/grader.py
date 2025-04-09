0f22c6d19dc4b3cec3faf67c3ef514dd4607f7b2
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-09 07:50:58 -0300

    Refactored grader and final_score.py for better structure.

--- grading/grader.py
+++ grading/grader.py
@@ -7,13 +7,14 @@ from utils.collector import TestCollector
 
 def get_test_results(test_file: str):
     collector = TestCollector()
-    result = pytest.main([test_file],plugins=[collector])
+    result = pytest.main([test_file,"-p","no:terminal"],plugins=[collector])
     passed_tests =  collector.passed
     failed_tests = collector.failed
     return passed_tests, failed_tests
 
 def get_score(passed_tests,total_tests:int):
     return (len(passed_tests)/total_tests)*100
+
 
 
 
